53525ddf0fd305e0437894e24d5e5bec406100ae
harvey <harvey@henghung.com>
2021-03-20 17:12:33 +0800

    update cors setting

--- index.js
+++ index.js
@@ -18,38 +18,18 @@ const coupon = require('./route/coupon/index.js');
 const store = require('./route/store/index.js');
 const activity = require('./route/activity/index.js');
 
+app.listen(port);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-const whitelist = [
-   'http://localhost:8080', 
-   'https://harvey0623.github.io',
-   'https://mmrm-server.herokuapp.com'
-];
-
-const corsOptions = {
+app.use(cors({
    origin(origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-         callback(null, true)
-      } else {
-         // callback(new Error('Not allowed by CORS'))
-         callback(null, true);
-      }
+      callback(null, true);
    },
    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
    credentials: true
-};
-
-app.use(cors(corsOptions));
-
-// app.use(cors({
-//    origin: 'http://localhost:8080',
-//    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//    credentials: true
-// }));
-app.listen(port);
+}));
 
 app.use('/member', member);
 app.use('/term', term);
