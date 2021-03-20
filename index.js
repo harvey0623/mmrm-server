173d5f8823f5f1a2f72dcba5f52f68c382431f23
harvey <harvey@henghung.com>
2021-03-20 12:05:48 +0800

    修改cors設定

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ const corsOptions = {
       if (whitelist.indexOf(origin) !== -1) {
          callback(null, true)
       } else {
-         callback(new Error('Not allowed by CORS'))
+         // callback(new Error('Not allowed by CORS'))
+         callback(null, true);
       }
    },
    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
